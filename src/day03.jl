94b8bca5f49d3c4a837e2d1f31fcf5dc5d3a84bf
Bill <wherrera10@users.noreply.github.com>
2025-12-02 20:34:12 -1000

    Refactor day03 function to use bdigits and @view

--- src/day03.jl
+++ src/day03.jl
@@ -5,18 +5,18 @@ using BenchmarkTools
 function day03()
 	part = [0, 0]
 	lines = split(read("day03.txt", String), '\n')
-	digits = [parse.(Int, collect(line)) for line in lines if !isempty(line)]
+	bdigits = [parse.(Int, collect(line)) for line in lines if !isempty(line)]
 
-	for row in digits
-		d1, dipos = findmax(row[begin:(end-1)])
+	for row in bdigits
+		d1, dipos = findmax(@view row[begin:(end-1)])
 		d2 = maximum(row[(dipos+1):end])
 		part[1] += 10 * d1 + d2
 	end
 
-	for row in digits
+	for row in bdigits
 		joltage, pos = 0, 1
 		for battery in 1:12
-			d, newpos = findmax(row[pos:(end-12+battery)])
+			d, newpos = findmax(@view row[pos:(end-12+battery)])
 			joltage = joltage * 10 + d
 			pos += newpos
 		end
