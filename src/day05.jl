95eabbd0e70e132cdb0258bd08b16588f644331c
Bill <wherrera10@users.noreply.github.com>
2025-12-04 23:37:55 -1000

    Refactor day05 function to streamline range handling

--- src/day05.jl
+++ src/day05.jl
@@ -3,14 +3,30 @@ using BenchmarkTools
 function day05()
     part = [0, 0]
     ranges = UnitRange{Int}[]
+    combined = UnitRange{Int}[]
 
     for line in eachline("day05.txt")
         if contains(line, '-')
             start, stop = parse.(Int, split(line, '-'))
             push!(ranges, start:stop)
-        elseif !isempty(line)
+        elseif isempty(line) && isempty(combined)
+            sortedranges = sort(ranges, by = r -> first(r))
+            start = first(sortedranges[1])
+            stop = last(sortedranges[1])
+            for r in Iterators.drop(sortedranges, 1)
+                if first(r) <= stop + 1
+                    stop = max(stop, last(r))
+                else
+                    push!(combined, start:stop)
+                    start = first(r)
+                    stop = last(r)
+                end
+            end
+            push!(combined, start:stop)
+            part[2] = sum(length, combined)
+        else
             num = parse(Int, line)
-            for (i, r) in enumerate(ranges)
+            for r in combined
                 if num in r
                     part[1] += 1
                     break
@@ -18,22 +34,6 @@ function day05()
             end
         end
     end
-
-    sortedranges = sort(ranges, by = r -> first(r))
-    combined = UnitRange{Int}[]
-    start = first(sortedranges[1])
-    stop = last(sortedranges[1])
-    for r in Iterators.drop(sortedranges, 1)
-        if first(r) <= stop + 1
-            stop = max(stop, last(r))
-        else
-            push!(combined, start:stop)
-            start = first(r)
-            stop = last(r)
-        end
-    end
-    push!(combined, start:stop)
-    part[2] = sum(length, combined)
 
     return part # [615, 353716783056994]
 end
