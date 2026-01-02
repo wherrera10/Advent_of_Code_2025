64491be81e76a8cfaa994de9a107066d8d9d858e
Bill <wherrera10@users.noreply.github.com>
2026-01-02 12:48:52 -1000

    Refactor day02 function and add constants
    Refactor day02 function to include new constants and improve logic for counting IDs.

--- src/day02.jl
+++ src/day02.jl
@@ -1,28 +1,34 @@
-# Advent of Code 2025, Day 2
-
 using BenchmarkTools
 
-function day02()
-    part = [0, 0]
-    dig = zeros(Int, 12)
-    for range in split(read("day02.txt", String), ',')
-        start, stop = parse.(Int, split(range, '-'))
-        for i in start:stop
-            ndig = ndigits(i)
-            half = ndig ÷ 2
-            digits!(dig, i)
-            for span in 1:half
-                ndig % span != 0 && continue
-                if all(dig[j] == dig[k] for j in 1:span for k in j+span:span:ndig)
-                    part[2] += i
-                    if span == half && iseven(ndig)
-                        part[1] += i
-                    end
-                    break
-                end
+const DAY2_1 = [[2, 1], [4, 2], [6, 3], [8, 4], [10, 5]]
+const DAY2_2 = [[3, 1], [5, 1], [6, 2], [7, 1], [9, 3], [10, 2]]
+const DAY2DUPS = [[6, 1], [10, 1]]
+const POW10 = [10^i for i in 0:12]
+
+"""
+Sum the number of values contained in the given id range for each type of grouping.
+"""
+function badidcount02(groupings, inputranges)
+    result = 0
+    for (rlen, glen) in groupings
+        interval  = (POW10[rlen + 1] - 1) ÷ (POW10[glen + 1] - 1)
+        for (rstart, rstop) in inputranges
+            lower = cld(max(rstart, POW10[rlen]), interval)
+            upper = min(rstop, POW10[rlen + 1]) ÷ interval
+            if lower ≤ upper
+                result +=  interval * (upper^2 - lower^2 + lower + upper) ÷ 2
             end
         end
     end
+    return result
+end
+
+function day02()
+    part = [0, 0]
+    nums = [parse(Int, s.match) for s in eachmatch(r"\d+", read("day02.txt", String))]
+    ranges = [[nums[i], nums[i+1]] for i in 1:2:length(nums)]
+    part[1] = badidcount02(DAY2_1, ranges)
+    part[2] = part[1] + badidcount02(DAY2_2, ranges) - badidcount02(DAY2DUPS, ranges)
     return part
 end
 
